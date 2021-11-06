751a2032b24f4b767188de5541a69724140e57e7
eduardomacetas <eduardo.macetas@gmail.com>
2021-11-05 22:57:35 -0500

    using 'for... of'

--- elyses_looping_enchantments.js
+++ elyses_looping_enchantments.js
@@ -10,5 +10,24 @@ function cardType(cards, cardType) {
   });
   return numberOfCards;
 }
-
 console.log(cardType([1, 2, 3, 4], 3)); // => 1
+
+// 2. Determine how many odd or even cards there are
+function determineOddEvenCards(cards, even) {
+  let numberOfCards = 0;
+  for (let card of cards) {
+    if (even == true) {
+      if (card % 2 == 0) {
+        numberOfCards += 1;
+      }
+    } else {
+      if (card % 2 != 0) {
+        numberOfCards += 1;
+      }
+    }
+  }
+  return numberOfCards;
+}
+
+console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], true));
+console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], false));
