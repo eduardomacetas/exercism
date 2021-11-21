f479bfbf3c5d5816f3485a51a2c0bd6183549e71
eduardomacetas <eduardo.macetas@gmail.com>
2021-11-20 22:08:50 -0500

    Remove comments

--- 01 Lucian's Luscious Lasagna/lasagna.js
+++ 01 Lucian's Luscious Lasagna/lasagna.js
@@ -1,35 +1,20 @@
-// Lucian's girlfriend is on her way home and he hasn't cooked their anniversary dinner!
-// In this exercise, you're going to write some code to help Lucian cook an exquisite lasagna from his favorite cook book.
-// You have four tasks, all related to the time spent cooking the lasagna.
+// 1. Define the expected oven time in minutes
+const EXPECTED_MINUTES_IN_OVEN = 40;
 
-// Define the EXPECTED_MINUTES_IN_OVEN constant that represents how many minutes
-// the lasagna should be in the oven. It must be exported. According to the cooking book,
-// the expected oven time in minutes is 40.
-
-export const EXPECTED_MINUTES_IN_OVEN = 40;
-
-// Implement the remainingMinutesInOven function that takes the actual minutes the lasagna has been in the oven
-// as a parameter and returns how many minutes the lasagna still has to remain in the oven,
-// based on the expected oven time in minutes from the previous task.
-
+// 2. Calculate the remaining oven time in minutes
 function remainingMinutesInOven(actualMinutesInOven) {
   return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
+console.log(remainingMinutesInOven(30)); // => 10
 
-// Implement the preparationTimeInMinutes function that takes the number of layers you added to the lasagna
-// as a parameter and returns how many minutes you spent preparing the lasagna,
-// assuming each layer takes you 2 minutes to prepare.
-
+// 3. Calculate the preparation time in minutes
 function preparationTimeInMinutes(numberOfLayers) {
   return numberOfLayers * 2;
 }
+console.log(preparationTimeInMinutes(2)); // => 4
 
-// Implement the totalTimeInMinutes function that takes two parameters:
-// the numberOfLayers parameter is the number of layers you added to the lasagna,
-// and the actualMinutesInOven parameter is the number of minutes the lasagna has been in the oven.
-// The function should return how many minutes in total you've worked on cooking the lasagna,
-// which is the sum of the preparation time in minutes, and the time in minutes the lasagna has spent in the oven at the moment.
-
+// 4. Calculate the total working time in minutes
 function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
   return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }
+console.log(totalTimeInMinutes(3, 20)); // => 26
