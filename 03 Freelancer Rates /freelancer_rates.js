a89ee9fd79b960134bc0c4f3d36985f294d49f92
eduardomacetas <eduardo.macetas@gmail.com>
2021-12-17 06:47:33 -0500

    Improve code  "freelancer_rates"

--- 03 Freelancer Rates /freelancer_rates.js
+++ 03 Freelancer Rates /freelancer_rates.js
@@ -4,3 +4,26 @@ function dayRate(hourlyRate) {
 }
 
 console.log(dayRate(89)); // 712
+
+// 2. Calculate the number of workdays given a fixed budget
+function daysInBudget(fixedBudget, hourlyRate) {
+  return Math.round(fixedBudget / dayRate(hourlyRate));
+}
+
+console.log(daysInBudget(20000, 89));
+
+// 3. Calculate the discounted rate for large projects
+function discount(value, percentage) {
+  return (value * percentage) / 1;
+}
+
+function priceWithMonthlyDiscount(hourlyRate, projectSpan, discountRate) {
+  const months = Math.trunc(projectSpan / 22);
+  const total = dayRate(hourlyRate) * projectSpan;
+  const cashDiscount =
+    discount(dayRate(hourlyRate) * 22, discountRate) * months;
+
+  return Math.round(total - cashDiscount);
+}
+
+console.log(priceWithMonthlyDiscount(89, 230, 0.42));
