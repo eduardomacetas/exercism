519630117a935c22e1707d71b703a073f409a618
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-01 11:21:32 -0500

    Learning 2 concepts: 'Increment/Decrement' and 'For Loops'

--- 07. Bid Watcher/bird_watcher.js
+++ 07. Bid Watcher/bird_watcher.js
@@ -1,37 +1,37 @@
-// More information: https://exercism.org/tracks/javascript/exercises/bird-watcher
-
 // 1. Determine the total number of birds that you counted so far
 function totalBirdCount(birdsPerDay) {
-  let totalBirds = 0;
+  let totalBird = 0;
   for (let i = 0; i < birdsPerDay.length; i++) {
-    totalBirds += birdsPerDay[i];
+    totalBird = totalBird + birdsPerDay[i];
   }
-  return totalBirds;
+  return totalBird;
 }
 
-birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
-console.log(totalBirdCount(birdsPerDay)); // 34
+// birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+console.log(totalBirdCount([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1])); // => 34
 
 // 2. Calculate the number of visiting birds in a specific week
 function birdsInWeek(birdsPerDay, week) {
-  let totalBirds = 0;
-  for (let i = 7 * (week - 1); i < 7 * week; i++) {
-    console.log(birdsPerDay[i]);
-    totalBirds += birdsPerDay[i];
+  const start = (week - 1) * 7;
+  const end = week * 7;
+  let totalBird = 0;
+  for (let i = start; i < end; i++) {
+    totalBird = totalBird + birdsPerDay[i];
   }
-  return totalBirds;
+  return totalBird;
 }
 
-birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
-console.log(birdsInWeek(birdsPerDay, 2)); // => 12
+// birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+console.log(birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2)); // => 12
 
 // 3. Fix a counting mistake
 function fixBirdCountLog(birdsPerDay) {
+  let fixBirdCount = [...birdsPerDay];
   for (let i = 0; i < birdsPerDay.length; i += 2) {
-    birdsPerDay[i] = birdsPerDay[i] + 1;
+    fixBirdCount[i] = birdsPerDay[i] + 1;
   }
-  return birdsPerDay;
+  return fixBirdCount;
 }
 
-birdsPerDay = [2, 5, 0, 7, 4, 1];
-console.log(fixBirdCountLog(birdsPerDay)); // => [3, 5, 1, 7, 5, 1]
+// birdsPerDay = [2, 5, 0, 7, 4, 1];
+console.log(fixBirdCountLog([2, 5, 0, 7, 4, 1])); // => [3, 5, 1, 7, 5, 1]
