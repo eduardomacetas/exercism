217b97926321853bcee2b7915de89914061171f6
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-01 21:21:23 -0500

    Exercises with 'While Loops' and 'Switch Statement'

--- 08. Mixed Juice/mixed_juice.js
+++ 08. Mixed Juice/mixed_juice.js
@@ -1,57 +1,73 @@
-// More information:https://exercism.org/tracks/javascript/exercises/mixed-juices
-
 // 1. Determine how long it takes to mix a juice
 function timeToMixJuice(juice) {
   switch (juice) {
     case "Pure Strawberry Joy":
       return 0.5;
-    case "Energizer":
+      break;
+    case "Energizer and Green Garden":
       return 1.5;
-    case "Green Garden":
-      return 1.5;
+      break;
     case "Tropical Island":
       return 3;
+      break;
     case "All or Nothing":
       return 5;
+      break;
     default:
       return 2.5;
+      break;
   }
 }
 
-console.log(timeToMixJuice("Tropical Island"));
-console.log(timeToMixJuice("Berries & Lime"));
+console.log(timeToMixJuice("Tropical Island")); // => 3
+console.log(timeToMixJuice("Berries & Lime")); // => 2.5
 
 // 2. Replenish the lime wedge supply
-function limesToCut(limeWedges, array) {
-  let wedgeCut = 0; // cantidad de litros
-  let limes = 0; // cantidad de limones
-  while (wedgeCut < limeWedges) {
-    if (array[limes] === "small") {
-      limes += 1;
-      wedgeCut += 6;
-    } else if (array[limes] == "medium") {
-      limes += 1;
-      wedgeCut += 8;
-    } else {
-      limes += 1;
-      wedgeCut += 10;
+function limesToCut(limeWedges, limes) {
+  let numberOfLimes = 0;
+  let numberOfLimesWedges = 0;
+  let i = 0;
+
+  while (numberOfLimesWedges <= limeWedges) {
+    switch (limes[i]) {
+      case "small":
+        numberOfLimesWedges += 6;
+        numberOfLimes++;
+        i++;
+        break;
+      case "medium":
+        numberOfLimesWedges += 8;
+        numberOfLimes++;
+        i++;
+        break;
+      case "large":
+        numberOfLimesWedges += 10;
+        numberOfLimes++;
+        i++;
+        break;
     }
   }
-  return limes;
+  return numberOfLimes;
 }
 
 console.log(limesToCut(25, ["small", "small", "large", "medium", "small"])); // => 4
 
 // 3. Finish up the shift
+function remainingOrders(time, juices) {
+  let timeSpend = 0;
+  let i = 0;
+  let unpreparedJuices = [...juices];
 
-function remainingOrders(timeLeft, orders) {
-  while (timeLeft > 0) {
-    timeLeft -= timeToMixJuice(orders);
-    orders.shift();
+  while (timeSpend <= time) {
+    timeSpend = timeToMixJuice(juices[i]);
+    unpreparedJuices.shift();
+    i++;
+    timeSpend++;
   }
-  return orders;
+
+  return unpreparedJuices;
 }
 
 console.log(
   remainingOrders(5, ["Energizer", "All or Nothing", "Green Garden"])
-);
+); // => ['Green Garden']
