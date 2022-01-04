251d390ac38338bf97ec34ba85901c3ce2213a20
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-03 22:04:58 -0500

    Calculate the sum for the numbers on the slot machine

--- 09. Lucky Numbers/lucky_numbers.js
+++ 09. Lucky Numbers/lucky_numbers.js
@@ -1,41 +1,8 @@
-// More information:https://exercism.org/tracks/javascript/exercises/lucky-numbers
-
 // 1. Calculate the sum for the numbers on the slot machine
 function twoSum(number1, number2) {
-  return Number(number1.join("")) + Number(number2.join(""));
+  let numberA = Number(String(number1).replace(/,/g, ""));
+  let numberB = Number(String(number2).replace(/,/g, ""));
+  return numberA + numberB;
 }
 
-console.log(twoSum([1, 2, 3], [0, 7]));
-
-// 2. Determine if a number is a palindrome
-function luckyNumber(number) {
-  let num = number.toString();
-  for (let i = 0; i < num.length; i++) {
-    console.log(".>>", num.length);
-    console.log("num[i]", num[i]);
-    console.log("num[num.length - i]", num[num.length - i - 1]);
-    if (num[i] == num[num.length - i - 1]) {
-      return true;
-    } else return false;
-  }
-}
-
-console.log(luckyNumber(1441));
-console.log(luckyNumber(123));
-
-// 3. Generate an error message for invalid user input
-function errorMessage(userInput) {
-  if (userInput == false) {
-    return "Required field";
-  }
-
-  if (isNaN(userInput)) {
-    return "''";
-  } else {
-    return "Must be a number besides 0";
-  }
-}
-
-console.log(errorMessage(""));
-console.log(errorMessage("123"));
-console.log(errorMessage("abc"));
+console.log(twoSum([1, 2, 3], [0, 7])); //=> 130
