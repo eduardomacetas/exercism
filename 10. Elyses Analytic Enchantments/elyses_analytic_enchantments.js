af7dbb058036f1ab321e36a8aa986cd0d0470dea
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-11 07:46:58 -0500

    Exercises with Arrays

--- 10. Elyses Analytic Enchantments/elyses_analytic_enchantments.js
+++ 10. Elyses Analytic Enchantments/elyses_analytic_enchantments.js
@@ -1,65 +1,65 @@
-// More information: https://exercism.org/tracks/javascript/exercises/elyses-analytic-enchantments
-
 // 1. Find the position of a card
 function getCardPosition(stack, card) {
   for (let i = 0; i < stack.length; i++) {
-    if (stack[i] === card) {
-      return i;
-    }
+    if (stack[i] === card) return i;
   }
+  return "Is not found";
 }
 
-const card = 2;
-console.log(getCardPosition([9, 7, 3, 2], card));
+// card = 2;
+console.log(getCardPosition([9, 7, 3, 2], 2)); // => 3
 
-// 2. Determine if a card is present
 function doesStackIncludeCard(stack, card) {
   for (let i = 0; i < stack.length; i++) {
-    return stack[i] === card ? true : false;
+    if (stack[i] === card) return true;
   }
+  return false;
 }
 
-console.log(doesStackIncludeCard([2, 3, 4, 5], 3));
+// card = 3;
+console.log(doesStackIncludeCard([2, 3, 4, 5], 3)); // => true
 
 // 3. Determine if each card is even
-function isEachCardEven(cards) {
-  let isEven = true;
-
-  for (let i = 0; i < cards.length; i++) {
-    cards[i] % 2 === 0 ? (isEven = true) : (isEven = false);
+function isEachCardEven(stack) {
+  let result = false;
+  for (let i = 0; i < stack.length; i++) {
+    stack[i] % 2 === 0 ? (result = true) : (result = false);
   }
-  return isEven;
+  return result;
 }
 
-console.log(isEachCardEven([2, 4, 6, 7]));
+console.log(isEachCardEven([2, 4, 6, 7])); // => false
 
 // 4. Check if the stack contains an odd-value card
-function doesStackIncludeOddCard(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    return cards[i] % 2 !== 0 ? true : false;
+function doesStackIncludeOddCard(stack) {
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i] % 2 !== 0) {
+      return true;
+    }
   }
+  return false;
 }
 
-console.log(doesStackIncludeOddCard([3, 2, 6, 4, 8]));
+console.log(doesStackIncludeOddCard([3, 2, 6, 4, 8])); // => true
 
 // 5. Get the first odd card from the stack
-function getFirstOddCard(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    if (cards[i] % 2 !== 0) {
-      return cards[i];
+function getFirstOddCard(stack) {
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i] % 2 !== 0) {
+      return stack[i];
     }
   }
+  return "there is no odd card";
 }
 
-console.log(getFirstOddCard([4, 2, 8, 7, 9]));
+console.log(getFirstOddCard([4, 2, 8, 7, 9])); // => 7
 
-// 6.  Determine the position of the first card that is even
-function getFirstEvenCardPosition(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    if (cards[i] % 2 === 0) {
-      return i;
-    }
+// 6. Determine the position of the first card that is even
+function getFirstEvenCardPosition(stack) {
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i] % 2 === 0) return i;
   }
+  return "there is no even card";
 }
 
-console.log(getFirstEvenCardPosition([5, 2, 3, 1]));
+console.log(getFirstEvenCardPosition([5, 2, 3, 1])); // => 1
