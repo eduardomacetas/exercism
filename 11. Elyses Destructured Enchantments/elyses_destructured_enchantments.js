5f5a1ba9b3384cc2b3c202b28e9fc25c178d7a64
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-11 22:17:19 -0500

    Exercises with Array Destructuring and Rest-Spread

--- 11. Elyses Destructured Enchantments/elyses_destructured_enchantments.js
+++ 11. Elyses Destructured Enchantments/elyses_destructured_enchantments.js
@@ -1,46 +1,44 @@
-// More information: https://exercism.org/tracks/javascript/exercises/elyses-destructured-enchantments
-
 // 1. Get the first card
 function getFirstCard(deck) {
-  let [x, ...args] = deck;
-  return x;
+  const [fistCard] = deck;
+  return fistCard;
 }
 
-const deck1 = [5, 9, 7, 1, 8];
-console.log(getFirstCard(deck1)); // => 5
+// const deck = [5, 9, 7, 1, 8];
+console.log(getFirstCard([5, 9, 7, 1, 8])); // => 5
 
 // 2. Get the second card
 function getSecondCard(deck) {
-  let [x, y, ...args] = deck;
-  return y;
+  const [, secondCard] = deck;
+  return secondCard;
 }
 
-const deck2 = [3, 2, 10, 6, 7];
-console.log(getSecondCard(deck2)); // => 2
+// const deck = [3, 2, 10, 6, 7];
+console.log(getSecondCard([3, 2, 10, 6, 7])); // => 2
 
 // 3. Swap the first two cards
 function swapTopTwoCards(deck) {
-  let [x, y, ...cards] = deck;
-  return [y, x, ...cards];
+  const [first, second, ...last] = deck;
+  return [second, first, ...last];
 }
 
-const deck3 = [10, 7, 3, 8, 5];
-console.log(swapTopTwoCards(deck3)); // => [7, 10, 3, 8, 5]
+// const deck = [10, 7, 3, 8, 5];
+console.log(swapTopTwoCards([10, 7, 3, 8, 5])); // => [7, 10, 3, 8, 5]
 
 // 4. Discard the top card
 function discardTopCard(deck) {
-  let [x, ...cards] = deck;
-  return [x, cards];
+  const [first, ...last] = deck;
+  return [first, [...last]];
 }
 
-const deck4 = [2, 5, 4, 9, 3];
-console.log(discardTopCard(deck4)); // => [2, [5, 4, 9, 3]]
+// const deck = [2, 5, 4, 9, 3];
+console.log(discardTopCard([2, 5, 4, 9, 3])); // => [2, [5, 4, 9, 3]]
 
 // 5. Insert face cards
 function insertFaceCards(deck) {
-  let [x, ...cards] = deck;
-  return [x, "jack", "queen", "king", ...cards];
+  const [first, ...last] = deck;
+  return [first, "jack", "queen", "king", ...last];
 }
 
-const deck5 = [5, 4, 7, 10];
-console.log(insertFaceCards(deck5)); // => [5, 'jack', 'queen', 'king', 4, 7, 10]
+// const deck = [5, 4, 7, 10];
+console.log(insertFaceCards([5, 4, 7, 10])); // => [5, 'jack', 'queen', 'king', 4, 7, 10]
