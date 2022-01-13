8cad8d5ce7d45a4ce7277815b91552a8c0a5e2e1
eduardomacetas <eduardo.macetas@gmail.com>
2022-01-12 22:46:21 -0500

    Exercises with 'Array Loops'

--- 12. Elyses Looping Enchantments/elyses_looping_enchantments.js
+++ 12. Elyses Looping Enchantments/elyses_looping_enchantments.js
@@ -1,33 +1,29 @@
-// More information: https://exercism.org/tracks/javascript/exercises/elyses-looping-enchantments
-
 // 1. Determine how many cards of a certain type are in the deck
-function cardType(cards, cardType) {
-  let numberOfCards = 0;
-  cards.forEach((element) => {
-    if (element === cardType) {
-      numberOfCards += 1;
+function cardTypeCheck(deck, card) {
+  let result = 0;
+  deck.forEach((e) => {
+    if (e === card) {
+      result = result + 1;
     }
   });
-  return numberOfCards;
+  return result;
 }
-console.log(cardType([1, 2, 3, 4], 3)); // => 1
+
+// const cardType = 3;
+console.log(cardTypeCheck([1, 2, 3, 4], 3)); // => 1
 
 // 2. Determine how many odd or even cards there are
-function determineOddEvenCards(cards, even) {
-  let numberOfCards = 0;
-  for (let card of cards) {
-    if (even == true) {
-      if (card % 2 == 0) {
-        numberOfCards += 1;
-      }
+function determineOddEvenCards(deck, isOdd) {
+  let result = 0;
+  for (const element of deck) {
+    if (isOdd) {
+      if (element % 2 === 0) result = result + 1;
     } else {
-      if (card % 2 != 0) {
-        numberOfCards += 1;
-      }
+      if (element % 2 !== 0) result = result + 1;
     }
   }
-  return numberOfCards;
+  return result;
 }
 
-console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], true));
-console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], false));
+console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], true)); // => 2
+console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], false)); // => 4
